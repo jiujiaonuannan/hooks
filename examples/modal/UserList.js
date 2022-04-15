3fd210be9cb3ae186630dfa174c2d7b2e1d562e8
alvin.wang <alvin.wang@binance.com>
2022-04-15 16:06:29 +0800

    chore: commit modal

---
+++ examples/modal/UserList.js
@@ -0,0 +1,54 @@
+import { useMemo, useState } from "react";
+import _ from "lodash";
+import { Button, Table } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+import { useNiceModal } from "./NiceModal";
+import data from "../10/data";
+
+export default () => {
+  const { show: showModal } = useNiceModal("user-info-modal");
+  const [users, setUsers] = useState(data.slice(0, 5));
+  const columns = useMemo(() => {
+    return [
+      {
+        title: "Name",
+        dataIndex: "name",
+      },
+      {
+        title: "Job Title",
+        dataIndex: "job",
+      },
+      {
+        title: "Actions",
+        render(value, user) {
+          return (
+            <Button
+              type="link"
+              icon={<EditOutlined />}
+              onClick={() => {
+                showModal({ user }).then((newUser) => {
+                  setUsers((users) => {
+                    // Modify users immutablly
+                    const byId = _.keyBy(users, "id");
+                    byId[newUser.id] = newUser;
+                    console.log('byId', _.values(byId))
+                    return _.values(byId);
+                  });
+                });
+              }}
+            />
+          );
+        },
+      },
+    ];
+  }, [showModal]);
+
+  return (
+    <Table
+      size="small"
+      pagination={false}
+      columns={columns}
+      dataSource={users}
+    />
+  );
+};
